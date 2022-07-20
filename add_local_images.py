1ad327e78c3cba687b583056dba75dd173decabd
Nick <nicholas.rosa@monash.edu>
2022-07-20 11:24:08 +1000

    add local images now works from a directory rather than the JSON files

--- add_local_images.py
+++ add_local_images.py
@@ -1,45 +1,113 @@
-import json, argparse, shutil, os
+import argparse, shutil, os
 import numpy as np
 
 LABEL_DIST = {
-    'Clear' : {
+    'clear' : {
         'uniform' : 0.25,
         'marco' : 0.336
     },
-    'Crystals' : {
+    'crystals' : {
         'uniform' : 0.25,
         'marco' : 0.128
     },
-    'Precipitate' : {
+    'precipitate' : {
         'uniform' : 0.25,
         'marco' : 0.48
     },
-    'Other' : {
+    'other' : {
         'uniform' : 0.25,
         'marco' : 0.056
     }
 }
 
+IMG_EXTENSIONS = (".jpg", ".jpeg", ".png", ".ppm", ".bmp", ".pgm", ".tif", ".tiff", ".webp")
+
 class Image(object):
-    def __init__(self, entropy, cross_entropy, filepath):
-        self.entropy = entropy
-        self.cross_entropy = cross_entropy
+    def __init__(self, filepath, label):
         self.filepath = filepath
-        self.label = filepath.split('/')[4].title()
+        self.label = label
 
     def __str__(self):
-        return f'filepath: {self.filepath}, label: {self.label}, entropy: {self.entropy}, cross_entropy: {self.cross_entropy}'
+        return f'filepath: {self.filepath}, label: {self.label}'
 
 
+def has_file_allowed_extension(filename: str, extensions) -> bool:
+    """Checks if a file is an allowed extension.
+
+    Args:
+        filename (string): path to a file
+        extensions (tuple of strings): extensions to consider (lowercase)
+
+    Returns:
+        bool: True if the filename ends with one of given extensions
+    """
+    return filename.lower().endswith(extensions if isinstance(extensions, str) else tuple(extensions))
 
 class ImageFactory(object):
-    def __init__(self, entropy_json_path, cross_entropy_json_path):
-        with open(entropy_json_path) as fp:
-            entropy_data = json.load(fp)
-        with open(cross_entropy_json_path) as fp:
-            cross_entropy_data = json.load(fp)   
+    def __init__(self, dataset_path):
+        self.labels, self.class_to_idx = self.find_classes(dataset_path)
+        samples = self.make_dataset(dataset_path, self.class_to_idx, IMG_EXTENSIONS)
 
-        self.images = [Image(x[0], y[0], x[1]) for x, y in zip(entropy_data, cross_entropy_data)]
+        self.idx_to_class = dict()
+        for key in self.class_to_idx:
+            self.idx_to_class[self.class_to_idx[key]] = key
+
+        self.images = [Image(x[0], self.idx_to_class[x[1]]) for x in samples]
+
+    def find_classes(self, directory: str):
+        classes = sorted(entry.name for entry in os.scandir(directory) if entry.is_dir())
+        if not classes:
+            raise FileNotFoundError(f"Couldn't find any class folder in {directory}.")
+
+        class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
+        return classes, class_to_idx
+
+
+    def make_dataset(
+        self,
+        directory,
+        class_to_idx = None,
+        extensions = None,
+    ):
+        """Generates a list of samples of a form (path_to_sample, class).
+
+        See :class:`DatasetFolder` for details.
+
+        Note: The class_to_idx parameter is here optional and will use the logic of the ``find_classes`` function
+        by default.
+        """
+        directory = os.path.expanduser(directory)
+
+        if extensions is not None:
+            def is_valid_file(x: str) -> bool:
+                return has_file_allowed_extension(x, extensions)  # type: ignore[arg-type]
+
+        instances = []
+        available_classes = set()
+        for target_class in sorted(class_to_idx.keys()):
+            class_index = class_to_idx[target_class]
+            target_dir = os.path.join(directory, target_class)
+            if not os.path.isdir(target_dir):
+                continue
+            for root, _, fnames in sorted(os.walk(target_dir, followlinks=True)):
+                for fname in sorted(fnames):
+                    path = os.path.join(root, fname)
+                    if is_valid_file(path):
+                        item = path, class_index
+                        instances.append(item)
+
+                        if target_class not in available_classes:
+                            available_classes.add(target_class)
+
+        empty_classes = set(class_to_idx.keys()) - available_classes
+        if empty_classes:
+            msg = f"Found no valid file for the classes {', '.join(sorted(empty_classes))}. "
+            if extensions is not None:
+                msg += f"Supported extensions are: {extensions if isinstance(extensions, str) else ', '.join(extensions)}"
+            raise FileNotFoundError(msg)
+
+        return instances
+
 
     def get_image_lst(self, label=None):
         if label is None:
@@ -48,15 +116,10 @@ class ImageFactory(object):
             return [x for x in self.images if x.label==label]
 
 
-def _copy_images(imgs, method, amount, destination):
+def _copy_images(imgs, amount, destination):
     assert len(imgs) >= amount
-    if method == 'random':
-        rng = np.random.default_rng()
-        rng.shuffle(imgs)
-    elif method == 'entropy':
-        imgs.sort(key=lambda x: x.entropy, reverse=True)
-    elif method == 'cross_entropy':
-        imgs.sort(key=lambda x: x.cross_entropy, reverse=True)
+    rng = np.random.default_rng()
+    rng.shuffle(imgs)
 
     for img in imgs[:amount]:
         shutil.copy(
@@ -67,22 +130,23 @@ def _copy_images(imgs, method, amount, destination):
     print(f"Copied {amount} images...  ")
 
 
-def add_images(method, image_cnt, label_dist, data_dir):
-    img_fact = ImageFactory('entropy.json', 'cross_entropy.json')
+def add_images(image_cnt, label_dist, input_dir, output_dir):
+    img_fact = ImageFactory(input_dir)
 
-    if label_dist == 'none':
+    if label_dist is None:
         images = img_fact.get_image_lst()
 
-        _copy_images(images, method, image_cnt, data_dir)
+        _copy_images(images, image_cnt, output_dir)
 
     else:
-        for label in ['Clear', 'Crystals', 'Precipitate', 'Other']:
+        for label in LABEL_DIST.keys():
             images = img_fact.get_image_lst(label=label)
+            print(label, label_dist)
             img_cnt = int(round(image_cnt * LABEL_DIST[label][label_dist]))
-            _copy_images(images, method, img_cnt, data_dir)
+            _copy_images(images, img_cnt, output_dir)
 
 def main(args):
-    add_images(args.method, args.image_cnt, args.label_dist, args.output_dir)
+    add_images(args.image_cnt, args.label_dist, args.input_dir, args.output_dir)
 
 
 
@@ -93,12 +157,12 @@ if __name__ == '__main__':
 
     # Strings
     parser.add_argument('--label_dist', type=str, choices=['none', 'uniform', 'marco'])
-    parser.add_argument('--method', type=str, choices=['random', 'entropy', 'cross_entropy'])
+    parser.add_argument('--image_cnt', type=int, default=1200)
+    parser.add_argument('--input_dir', type=str)
     parser.add_argument('--output_dir', type=str)
 
     
 
     args = parser.parse_args()
-    print(args)
 
     main(args)
